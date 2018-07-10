1e075cb0d7edf984e585380a7da01f0b25e55045
kecramer <kcramer@gmail.com>
2018-07-10 14:14:07 -0700

    Extremely basic first version that loads a local file (provided) and can check a single callsign

---
+++ index.js
@@ -0,0 +1,38 @@
+const xml2js = require('xml2js')
+const fs = require('fs')
+const events = require('events')
+
+module.exports = class DXCCjs extends events.EventEmitter{
+  constructor(path=null) {
+    super()
+
+    if(!path) {
+      this.prefixes = {}
+    } else {
+      this.loadPrefixes(path)
+    }
+  }
+
+  loadPrefixes(path) {
+    let parser = new xml2js.Parser()
+    fs.readFile(path, (err, data) => {
+      parser.parseString(data, (err, result) => {
+        if(err) {
+          console.log('An error occured parsing the given document')
+          return
+        }
+        this.prefixes = result.clublog.prefixes[0].prefix
+        this.emit('loaded')
+      })
+    })
+  }
+
+  checkCall(call) {
+    let foundPrefix = null;
+    for(let i = 0; i < this.prefixes.length; i++) {
+      if (call.indexOf(this.prefixes[i].call[0]) == 0) {
+        return this.prefixes[i]
+      }
+    }
+  }
+}
